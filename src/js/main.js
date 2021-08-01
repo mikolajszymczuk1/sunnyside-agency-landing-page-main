1c29c7aa566e0195d59ed54e1f1768aa1df98c9b
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-08-01 20:29:19 +0200

    Create basic script to controll navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-console.log("Hello World !!!");
+// Navigation menu controller
+const hamburger = document.querySelector(".navigation__hamburger");
+const links_container = document.querySelector(".navigation__links-container");
+
+hamburger.addEventListener("click", function() {
+    this.classList.toggle("navigation__hamburger--active");
+    links_container.classList.toggle("navigation__links-container--active");
+});
